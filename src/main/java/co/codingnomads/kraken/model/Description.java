2bd61735e9ff72e09d8c2e1bbcf26841ed2e6e71
Meg <the_meej@yahoo.com>
2017-11-30 10:43:58 -0500

    description class

---
+++ src/main/java/co/codingnomads/kraken/model/Description.java
@@ -0,0 +1,93 @@
+package co.codingnomads.kraken.model;
+
+public class Description {
+    /**
+     * Created by Meghan Boyce on 11/29/17
+     *
+     * order description info
+     */
+
+    // Asset pair
+    String pair;
+    // Type of order (buy/sell)
+    String type;
+    // TODO: ordertype = order type (See Add standard order, steal from Thomas)
+    // Primary price
+    String price;
+    // Secondary price
+    String price2;
+    // Amount of leverage
+    String leverage;
+    // Order description
+    String order;
+    // Conditional close order description (if conditional close set)
+    String close;
+
+    // Fully qualified Constructor
+    // TODO: Redo Constructor after completing ordertype above, create one w/out close
+    public Description(String pair, String type, String price, String price2, String leverage, String order, String close) {
+        this.pair = pair;
+        this.type = type;
+        this.price = price;
+        this.price2 = price2;
+        this.leverage = leverage;
+        this.order = order;
+        this.close = close;
+    }
+
+    public String getPair() {
+        return pair;
+    }
+
+    public void setPair(String pair) {
+        this.pair = pair;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPrice2() {
+        return price2;
+    }
+
+    public void setPrice2(String price2) {
+        this.price2 = price2;
+    }
+
+    public String getLeverage() {
+        return leverage;
+    }
+
+    public void setLeverage(String leverage) {
+        this.leverage = leverage;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getClose() {
+        return close;
+    }
+
+    public void setClose(String close) {
+        this.close = close;
+    }
+}
