e4390ad504fb4fa069b36a8faca1d87b5fe42fc3
Jialor Cheung <jialorcheung@gmail.com>
2017-12-01 11:09:47 -0800

    updated controller with GetTradableAssetPAirs test

--- src/main/java/co/codingnomads/kraken/Controller.java
+++ src/main/java/co/codingnomads/kraken/Controller.java
@@ -14,21 +14,13 @@ public class Controller {
 
         GetBalanceRequestBody a = new GetBalanceRequestBody();
         GenericRequestHandler handler = new GenericRequestHandler();
-<<<<<<< HEAD
-        OutputWrapper c = handler.callAPI(KrakenRequestEnum.GETSERVERTIME,null);
-        // System.out.println(c.getErrors()[0]);
-        System.out.println(((GetServerTimeOutput) c.getResult()).getUnixtime());
-        System.out.println(((GetServerTimeOutput) c.getResult()).getRfc1123());
 
         OutputWrapper d = handler.callAPI(KrakenRequestEnum.GETTRADABLEASSETPAIRS,null);
         System.out.println( d.getResult());
 
-
-
-=======
         OutputWrapper c = handler.callAPI(KrakenRequestEnum.GETACCOUNTBALANCE,a);
         System.out.println(c);
         System.out.println(c.getResult());
->>>>>>> 05a813337b97708d70b97568e5f517a3cf7f7df1
+
     }
 }
