768004e0c50c35e1ebaeadc8167417e44da14866
Ryan Desmond <radesmond@gmail.com>
2017-12-04 18:49:22 -0500

    stashing for the night

--- src/main/java/co/codingnomads/kraken/util/ArrayMapDeserializer.java
+++ src/main/java/co/codingnomads/kraken/util/ArrayMapDeserializer.java
@@ -42,26 +42,5 @@ public class ArrayMapDeserializer extends JsonDeserializer<Map<String, AssetPair
             return new HashMap<String, AssetPairName>();
         }
     }
-
-//    @Override
-//    public Map<String, AssetPairName> deserialize(JsonParser jsonParser, DeserializationContext arg1)
-//            throws IOException, JsonProcessingException {
-//
-//        ObjectCodec oc = jsonParser.getCodec();
-//        JsonNode node = oc.readTree(jsonParser);
-//
-//        if (node.getValueAsText() == "")
-//            return null;
-//
-//        MyObject myObject = new MyObject();
-//        myObject.setMyStirng(node.get("myString").getTextValue());
-//
-//        JsonNode childNode = node.get("childObject");
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        ChildObject childObject = objectMapper.readValue(childNode,
-//                ChildObject.class);
-//
-//        myObject.setChildObject(childObject);
-//        return myObject;
-//    }
+    
 }
